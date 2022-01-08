0117dca9fa9019c5bec223e23b7a0aa60d79d099
Weizhi <pengwz98@qq.com>
2022-01-08 00:47:51 +0000

    update js

--- file/functions.js
+++ file/functions.js
@@ -39,7 +39,10 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var seconds = (Date.parse(current)-Date.parse(date))/1000;
+	console.log(Date.parse(current));
+	console.log(Date.parse(date));
+	console.log(seconds);
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
